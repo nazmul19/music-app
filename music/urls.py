a1aba1229f3fbb7356e5d4e6dfde7f447b7ebe55
Nazmul Hassan <nazmul.hassan@demand-farm.com>
2018-03-07 18:23:28 +0530

    initial commit

---
+++ music/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+    # /music/
+    path('', views.index, name='index'), 
+    
+    # /music/71/
+    path('<int:album_id>/', views.detail, name='detail'),
+
+    # /music/album_id/favorite/
+    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
+]
